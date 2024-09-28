08d0852e33ebcafd5f552b37f1eb107b68677412
narroworb <dmitriy_bessonov03@mail.ru>
2024-09-28 23:40:37 +0300

    fix validation of rotation

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ busy_cells = 0
 points_counter = 0
 start_time = 0
 fall_speed = 5
-difficulty = 5
+difficulty = 20
 forms ={'I': [[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]],
         'J': [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0]],
         'L': [[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0]],
@@ -91,8 +91,8 @@ def create_grid(locked_position={}):
     grid = [[black for _ in range(16)] for _ in range(28)]
     for y in range(29):
         for x in range(17):
-            if (x * 25, y * 25 + 50) in locked_position:
-                grid[y-1][x] = locked_position[(x * 25, y * 25 + 50)]
+            if (x * rect_size, y * rect_size + 50) in locked_position:
+                grid[y-1][x] = locked_position[(x * rect_size, y * rect_size + 50)]
     return grid
 
 def valid_moving(cur_fig):
@@ -100,16 +100,16 @@ def valid_moving(cur_fig):
     for y in range(len(cur_fig.form)):
         for x in range(len(cur_fig.form[y])):
             if cur_fig.form[y][x] == 1:
-                if x * rect_size + cur_fig.x == 0 or (x * rect_size + cur_fig.x - rect_size, (y - 2) * rect_size + (cur_fig.y // 25) * 25) in locked_position.keys():
+                if x * rect_size + cur_fig.x == 0 or (x * rect_size + cur_fig.x - rect_size, (y - 2) * rect_size + (cur_fig.y // rect_size) * rect_size) in locked_position.keys():
                     extreme_left = True
-                if x * rect_size + cur_fig.x == 375 or (x * rect_size + cur_fig.x + rect_size, (y - 2) * rect_size + (cur_fig.y // 25) * 25) in locked_position.keys():
+                if x * rect_size + cur_fig.x == 375 or (x * rect_size + cur_fig.x + rect_size, (y - 2) * rect_size + (cur_fig.y // rect_size) * rect_size) in locked_position.keys():
                     extreme_right = True
                 if y * rect_size + cur_fig.y >= 825:
                     extreme_down = True
                     cur_fig.y = 825 - y * rect_size
-                if (x * rect_size + cur_fig.x, (y - 2) * rect_size + (cur_fig.y // 25) * 25) in locked_position.keys():
+                if (x * rect_size + cur_fig.x, (y - 2) * rect_size + (cur_fig.y // rect_size) * rect_size) in locked_position.keys():
                     extreme_down = True
-                    cur_fig.y = (cur_fig.y // 25) * 25 - 25
+                    cur_fig.y = (cur_fig.y // rect_size) * rect_size - rect_size
     return (extreme_left, extreme_right, extreme_down)
 
 def figure_move(event, validation):
@@ -128,15 +128,15 @@ def deleting_row(locked_position={}):
     locked_position_coordinates = []
     for (x, y) in locked_position.keys():
         locked_position_coordinates.append(y)
-    for y in range(0, 751, 25):
+    for y in range(0, 751, rect_size):
         if locked_position_coordinates.count(y) == 16:
             points_counter += 10
-            for x in range(0, 401, 25):
+            for x in range(0, 401, rect_size):
                 if (x, y) in locked_position.keys():
                     del locked_position[(x, y)]
-                for y_upper in range(y - 25, 150, -25):
+                for y_upper in range(y - rect_size, 150, -rect_size):
                     if (x, y_upper) in locked_position.keys():
-                        locked_position[(x, y_upper + 25)] = locked_position[(x, y_upper)]
+                        locked_position[(x, y_upper + rect_size)] = locked_position[(x, y_upper)]
                         del locked_position[(x, y_upper)]
     return locked_position
 
@@ -149,7 +149,7 @@ def restart():
     global locked_position, busy_cells, points_counter, start_time
     game_over_message = game_over_font.render('ИГРА ЗАКОНЧЕНА', True, colors['red'])
     game_over_points = game_over_font.render(f'Ваш счёт: {points_counter}', True, (255, 255, 255))
-    restart_font = pygame.font.Font('./fonts/Domino Italic.otf', 25)
+    restart_font = pygame.font.Font('./fonts/Domino Italic.otf', rect_size)
     restart_message = restart_font.render('Начать заново? Нажмите пробел!', True, random.choice(list(colors.values())))
     while 1:
         pygame.draw.rect(dis, black, (0, 0, display_width, display_height))
@@ -182,7 +182,7 @@ class Figure:
         self.rotation = 0
         self.color = random.choice(list(colors.values()))
 
-    def change_rotation(self):
+    def change_rotation(self, locked_position={}):
         copy_form = []
         for row in self.form:
             copy_row = []
@@ -207,6 +207,12 @@ class Figure:
         if (self.x + rect_size * min_left_prev) - (min_left_prev - min_left_post) * rect_size < 0:
             self.rotation = (self.rotation + 3) % 4
             return
+        for y in range(len(self.form)):
+            for x in range(len(self.form[y])):
+                if copy_form[y][x] == 1:
+                    if ((3 - x) * rect_size + cur_fig.x, y * rect_size + (cur_fig.y // rect_size) * rect_size) in locked_position.keys():
+                        self.rotation = (self.rotation + 3) % 4
+                        return
         for y in range(len(self.form)):
             for x in range(len(self.form[y])):
                 self.form[y][x] = copy_form[3 - x][y]
@@ -234,7 +240,7 @@ while not is_quit:
                 (dx, dy) = figure_move(event, validation_move)
                 if event.key == pygame.K_SPACE:
                     cur_fig.rotation = (cur_fig.rotation + 1) % 4
-                    cur_fig.change_rotation()
+                    cur_fig.change_rotation(locked_position)
         cur_grid = create_grid(locked_position)
         for y in range(len(cur_grid)):
             for x in range(len(cur_grid[y])):
@@ -247,7 +253,7 @@ while not is_quit:
             for y in range(4):
                 for x in range(4):
                     if cur_fig.form[y][x] == 1:
-                        locked_position[(cur_fig.x + x * 25, cur_fig.y - (3 - y) * rect_size)] = cur_fig.color
+                        locked_position[(cur_fig.x + x * rect_size, cur_fig.y - (3 - y) * rect_size)] = cur_fig.color
 
         min_y_by_x = [-1, -1, -1, -1]
         for y in range(4):
@@ -258,12 +264,12 @@ while not is_quit:
         for y in range(4):
             for x in range(4):
                 if cur_fig.form[y][x] == 1:
-                    if (cur_fig.x + x * 25, ((cur_fig.y + 25 * (min_y_by_x[x] - 2)) // 25) * 25) in locked_position.keys():
-                        locked_position[(cur_fig.x + x * 25, (cur_fig.y // 25 * 25) - (3 - min_y_by_x[x]) * rect_size)] = cur_fig.color
+                    if (cur_fig.x + x * rect_size, ((cur_fig.y + rect_size * (min_y_by_x[x] - 2)) // rect_size) * rect_size) in locked_position.keys():
+                        locked_position[(cur_fig.x + x * rect_size, (cur_fig.y // rect_size * rect_size) - (3 - min_y_by_x[x]) * rect_size)] = cur_fig.color
                         for j in range(4):
                             for i in range(4):
                                 if cur_fig.form[j][i] == 1:
-                                    locked_position[(cur_fig.x + i * 25, (cur_fig.y // 25 * 25) - (3 - j) * rect_size)] = cur_fig.color
+                                    locked_position[(cur_fig.x + i * rect_size, (cur_fig.y // rect_size * rect_size) - (3 - j) * rect_size)] = cur_fig.color
                         break
 
         if len(locked_position) > busy_cells:
